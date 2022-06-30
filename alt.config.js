87798b727f30caffd9e148b62b785a76253c7ae1
Adrià Marcet <adriamarcetrovira@gmail.com>
2022-06-30 14:47:16 +0200

    Initial commit

---
+++ alt.config.js
@@ -0,0 +1,14 @@
+const SITE_NAME = 'Archetype Landings Twig';
+const SITE_URL = 'https://alt.netlify.app';
+
+const defaultLanguage = 'en';
+const languages = ['en', 'es'];
+
+const routes = {
+  demo: {
+    es: 'ejemplo'
+  }
+};
+
+export { SITE_NAME, SITE_URL };
+export { defaultLanguage, languages, routes };
